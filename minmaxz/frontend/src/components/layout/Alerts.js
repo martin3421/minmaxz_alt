a62fd61e40b12e6719c23a23c3e9c723cfdb468e
Martin Herold <martin.herold@hs-karlsruhe.de>
2019-10-08 16:57:50 +0200

    messages for events

--- minmaxz/frontend/src/components/layout/Alerts.js
+++ minmaxz/frontend/src/components/layout/Alerts.js
@@ -1,13 +1,27 @@
 import React, { Component, Fragment } from "react";
 import { withAlert } from "react-alert";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
 export class Alerts extends Component {
-    componentDidMount() {
-        this.props.alert.show('It works');
+    static propTypes = {
+        error: PropTypes.object.isRequired
+      };
+    componentDidUpdate(prevProps) {
+        const { error, alert } = this.props;
+        if (error !== prevProps.error) {
+            if(error.msg.datum) alert.error(`Datum: ${error.msg.datum.join()}`);
+            if(error.msg.beschreibung) alert.error(`Beschreibung: ${error.msg.beschreibung.join()}`);
+            if(error.msg.betrag) alert.error(`Datum: ${error.msg.betrag.join()}`);
+        }
     }
     render() {
         return <Fragment />;
     }
 }
 
-export default withAlert()(Alerts);
+const mapStateToProps = state => ({
+    error: state.errors
+  });
+
+  export default connect(mapStateToProps)(withAlert()(Alerts));
--- minmaxz/frontend/src/reducers/index.js
+++ minmaxz/frontend/src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from "redux";
 import buchungen from './buchungen';
 import konten from './konten';
 import errors from './errors';
+import messages from './messages';
 
 
 export default combineReducers({
     buchungen,
     konten,
-    errors
+    errors,
+    messages
 });
