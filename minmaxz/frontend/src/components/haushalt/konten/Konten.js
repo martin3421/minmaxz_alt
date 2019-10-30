e57648911183f5c1b093e95dfafbc42fb04153a7
Martin Herold <martin.herold@hs-karlsruhe.de>
2019-10-30 16:47:40 +0100

    multilevel acc

--- minmaxz/frontend/src/components/haushalt/konten/Konten.js
+++ minmaxz/frontend/src/components/haushalt/konten/Konten.js
@@ -1,8 +1,53 @@
 import React, { Component, Fragment } from 'react'
-import { Accordion, Icon } from 'semantic-ui-react'
+import { Accordion, Icon, List } from 'semantic-ui-react'
 import { connect } from 'react-redux'
 import PropTypes from "prop-types";
 import { getKonten } from "../../../actions/konten";
+  
+  const Level1aaContent = (
+    <List>
+    <List.Item>DKB</List.Item>
+    <List.Item>Sparkasse</List.Item>
+    </List>
+  )
+
+const level1aPanels = [
+    { key: 'panel-1ab', title: 'Girokonto', content: {content: Level1aaContent} },
+  ]
+  
+  const Level1aContent = (
+    <List>
+    <List.Item>Bargeld</List.Item>
+    <List.Item><Accordion.Accordion panels={level1aPanels} /></List.Item>
+    </List>
+  )
+
+const level1Panels = [
+    { key: 'panel-1a', title: 'Barvermögen', content: {content: Level1aContent} },
+    { key: 'panel-ba', title: 'Geldanlagen', content: 'Level 1B Contents' },
+  ]
+  
+  const Level1Content = (
+    <div>
+      <Accordion.Accordion panels={level1Panels} />
+    </div>
+  )
+  
+  const level2Panels = [
+    { key: 'panel-2a', title: 'Level 2A', content: 'Level 2A Contents' },
+    { key: 'panel-2b', title: 'Level 2B', content: 'Level 2B Contents' },
+  ]
+  
+  const Level2Content = (
+    <div>
+      <Accordion.Accordion panels={level2Panels} />
+    </div>
+  )
+  
+  const rootPanels = [
+    { key: 'panel-1', title: 'Aktiva', content: { content: Level1Content } },
+    { key: 'panel-2', title: 'Aufwendungen', content: { content: Level2Content } },
+  ]
 
 export class Konten extends Component {
 
@@ -29,61 +74,12 @@ export class Konten extends Component {
         const { activeIndex } = this.state
 
         return (
-            <Accordion>
-                <Accordion.Title
-                    active={activeIndex === 0}
-                    index={0}
-                    onClick={this.handleClick}
-                >
-                    <Icon name='dropdown' />
-                    What is a dog?
-        </Accordion.Title>
-                <Accordion.Content active={activeIndex === 0}>
-                    <p>
-                        A dog is a type of domesticated animal. Known for its loyalty and
-                        faithfulness, it can be found as a welcome guest in many households
-                        across the world.
-          </p>
-                </Accordion.Content>
-
-                <Accordion.Title
-                    active={activeIndex === 1}
-                    index={1}
-                    onClick={this.handleClick}
-                >
-                    <Icon name='dropdown' />
-                    What kinds of dogs are there?
-        </Accordion.Title>
-                <Accordion.Content active={activeIndex === 1}>
-                    <p>
-                        There are many breeds of dogs. Each breed varies in size and
-                        temperament. Owners often select a breed of dog that they find to be
-                        compatible with their own lifestyle and desires from a companion.
-          </p>
-                </Accordion.Content>
-
-                <Accordion.Title
-                    active={activeIndex === 2}
-                    index={2}
-                    onClick={this.handleClick}
-                >
-                    <Icon name='dropdown' />
-                    How do you acquire a dog?
-        </Accordion.Title>
-                <Accordion.Content active={activeIndex === 2}>
-                    <p>
-                        Three common ways for a prospective owner to acquire a dog is from
-                        pet shops, private owners, or shelters.
-          </p>
-                    <p>
-                        A pet shop may be the most convenient way to buy a dog. Buying a dog
-                        from a private owner allows you to assess the pedigree and
-                        upbringing of your dog before choosing to take it home. Lastly,
-                        finding your dog from a shelter, helps give a good home to a dog who
-                        may not find one so readily.
-          </p>
-                </Accordion.Content>
-            </Accordion>
+            <Accordion
+            activeIndex={activeIndex}
+            panels={rootPanels}
+            styled
+            onTitleClick={this.handleClick}
+          />
         )
     }
 }
